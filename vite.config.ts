a69b3bcb762ac1f4e0b22111f974636876356f4b
vacrain <726866011@qq.com>
2022-08-22 16:52:30 +0800

    ✨  github action

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "./", // 默认打包后 index.html 中是 /assets, 设置本项后变成 ./assets
+  // base: "./", // 默认打包后 index.html 中是 /assets, 设置本项后变成 ./assets
+  base: "production" === process.env.NODE_ENV ? `/happyj-pro/` : "/",
+  build: {
+    outDir: "dist",
+  },
 });
