aad9cb02398d108301bb9767c261bfb930dfb534
vacrain <726866011@qq.com>
2022-08-22 14:12:21 +0800

    🎉  init

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: "./", // 默认打包后 index.html 中是 /assets, 设置本项后变成 ./assets
+});
