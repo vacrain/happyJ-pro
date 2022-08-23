0f04706749cb2c70596629900271981674aaa137
vacrain <726866011@qq.com>
2022-08-23 16:00:57 +0800

    🚀

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+  // This starter template is using Vue 3 <script setup> SFCs
+  // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+  import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -14,18 +14,42 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  123
+  <button
+    class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700 border-none cursor-pointer"
+  >
+    Click me
+  </button>
+
+  <hr />
+
+  <button
+    p="y-2 x-4"
+    font="semibold"
+    shadow="lg"
+    text="white"
+    bg="green-500 hover:green-700"
+    border="rounded-lg none "
+    cursor="pointer"
+  >
+    Click me
+  </button>
+
+  <hr />
+
+  <div class="i-ic-baseline-add-circle text-3xl bg-green-500" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import Unocss from 'unocss/vite'
+import { presetUno, presetAttributify, presetIcons } from 'unocss'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Unocss({
+      presets: [presetUno(), presetAttributify(), presetIcons()]
+    })
+  ],
   // base: "./", // 默认打包后 index.html 中是 /assets, 设置本项后变成 ./assets
   base: 'production' === process.env.NODE_ENV ? `/happyj-pro/` : '/',
   build: {
