4d85cc8afaee2b983fb1da279292f38acf134f37
vacrain <726866011@qq.com>
2022-08-23 08:31:55 +0800

    🚑

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   // base: "./", // 默认打包后 index.html 中是 /assets, 设置本项后变成 ./assets
-  // base: "production" === process.env.NODE_ENV ? `/happyj-pro/` : "/",
+  base: 'production' === process.env.NODE_ENV ? `/happyj-pro/` : '/',
   build: {
-    outDir: "dist",
-  },
-});
+    outDir: 'dist'
+  }
+})
